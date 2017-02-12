99839a9115367fa60e839eb41b5a8070517f7ff8
Alex Damaschin <ad@peytz.dk>
2017-02-12 11:37:05 +0200

    date +%Y-%m-%d:%H:%M:%S - Done content loading.

--- js/ting_search_ajaxify.js
+++ js/ting_search_ajaxify.js
@@ -1,55 +1,45 @@
 (function($) {
   'use strict';
 
-  // var svalue = $('input[name="search_block_form"]').val();
-
-  // window.history.pushState("string", "Title", svalue);
-  // $('.pane-search-result').ready(function() {
-  //
-  //   var svalue = Drupal.settings.TingSearchAjaxify.key;
-  //
-  //   $.ajax({
-  //     url: Drupal.settings.basePath + 'ajaxify/search/' + svalue,
-  //     success: function (response) {
-  //       search_wrapper.html("<div class='search-results-wrapper' id='search-results-wrapper'>" + response[3].data + "</div>");
-  //     }
-  //   });
-  // });
-
   Drupal.behaviors.TingSearchAjaxify = {
-    attach: function (context, settings) {
-      // Drupal.attachBehaviors('ding_facetbrowser');
-      $(document).trigger('facetBrowser');
-      // console.log('123123');
-      var svalue = $('input[name="search_block_form"]').val();
-      var search_wrapper = $('#search-results-wrapper');
-      var facetbrowser = $('#js-facet-browser-toggle');
-      var query = Drupal.settings.ding_facetbrowser.query;
+    attach: function(context, settings) {
 
-      window.history.pushState("string", "Title", query);
-      $.ajax({
-        url: Drupal.settings.basePath + 'ajaxify/search/' + query,
-        // type: 'POST',
-        // data: {
-        //   search_block_form: svalue
-        // },
-        success: function (response) {
-          // var results_count = response[1].data;
-          // $('.pane-search-result-count').html(results_count);
-          //
-          // // Return facets data.
-          // var facet_content = response[2].data;
-          // console.log(facet_content);
-          // facetbrowser.replaceWith(facet_content);
-          //
-          // // Return search results data.
-          // var search_content = response[3].data;
-          // // search_wrapper.html("<div class='search-results-wrapper' id='search-results-wrapper'>" + search_content + "</div>");
-          // search_wrapper.html(search_content);
-          //
-        }
+      $(document, context).once(function() {
+        $(this).load('ajaxify');
+      });
+      $(".pager a").each(function(){
+        $(".pager a").addClass("use-ajax");
       });
 
+      $('.pane-search-result .pane-content').attr('id', 'search-results-wrapper');
+      $('.pane-search-result-count .page-title').attr('id', 'search-count');
+
+
+
+      $('.search-results ul.pager li a').click(search_result_pager);
+
+      // Filter Results Paginations
+      function search_result_pager() {
+        var thisLink = $(this);
+        var thisUrl = getLocation(this.href);
+        var target = $("#search-results-wrapper");
+        $.ajax({
+          url: Drupal.settings.basePath + 'ajaxify' + thisUrl.search,
+          type: "GET",
+          success: function(data) {
+            console.log(data);
+            $(target).html(data[3].data);
+            // Drupal.attachBehaviors(".search-results");
+          }
+        });
+        return false;
+      }
+
+      var getLocation = function(href) {
+        var l = document.createElement("a");
+        l.href = href;
+        return l;
+      };
     }
   };
 
